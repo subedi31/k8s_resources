835350717d4cd2817ac794433a48ede29883edd6
subedi31 <amritsubedi3131@gmail.com>
2023-08-10 01:57:25 +0545

    Testing resources

--- modules/namespace/main.tf
+++ modules/namespace/main.tf
@@ -3,7 +3,7 @@ variable "namespace_name" {
   type        = string
   default     = "default"  # Change this to your desired default value
 }
-resource "helm_release" "namespace" {
+module "helm_release" "namespace" {
   chart     = "./helm-namespace"
   name      = "namespace-template"
   namespace = var.namespace_name
