a8e83dee37069c990c87c8e999c744c1e99b9f4f
subedi31 <amritsubedi3131@gmail.com>
2023-08-09 16:03:15 +0545

    add network policy

--- namespace/main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "helm_release" "resource-template" {
-  chart     = "./helm-namespace"  # Path to helm-namespace subdirectory
-  name      = "namespace-template"
-  values    = [file("./helm-namespace/values.yaml")]
-
-  
-}
