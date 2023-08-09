ba9670b57406c725279bdccb5c2b778d0d21d64b
subedi31 <amritsubedi3131@gmail.com>
2023-08-09 12:05:14 +0545

    add namespace

--- namespace/main.tf
+++ namespace/main.tf
@@ -2,7 +2,7 @@
 resource "helm_release" "resource-template" {
   chart     = "./helm-namespace"  # Path to helm-namespace subdirectory
   name      = "namespace-template"
-  values    = [file("./helm-namespace/values.yaml")]
+  values    = [file("./namespace/helm-namespace/values.yaml")]
 
   
 }
